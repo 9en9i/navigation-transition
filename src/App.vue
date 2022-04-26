b275aeb1cfaa88df1f73fe7a463c0347c05314d9
Denis Diveev <ddiveev99@gmail.com>
2022-04-26 20:30:56 +0300

    add dynamic resize

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="navigation" ref="navigation">
-    <div class="navigation__circle"/>
+    <div class="navigation__circle" :class="{'navigation__circle_animation': useTransition}"/>
     <div class="navigation__button-wrap">
       <button v-for="index in 4" class="navigation__button" @click="onClick(index)" :key="index">
         {{index}}
@@ -13,22 +13,37 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import {
+  ref, onUnmounted,
+} from 'vue';
 
 const clickedElement = ref(0);
-const onClick = (index) => {
-  clickedElement.value = index - 1;
-};
 
 const navigation = ref();
 
-const leftPadding = computed(() => {
-  if (clickedElement.value < 4) {
-    return `${5 + clickedElement.value * 80}px`;
-  }
-  return `${navigation.value.clientWidth - 75}px`;
-});
+const leftPadding = ref('5px');
 
+const onClick = (index) => {
+  clickedElement.value = index - 1;
+  if (clickedElement.value < 4) {
+    leftPadding.value = `${5 + clickedElement.value * 80}px`;
+  }
+  leftPadding.value = `${navigation.value.clientWidth - 75}px`;
+};
+const useTransition = ref(true);
+
+const resizeHandler = (e) => {
+  console.log(e);
+  if (clickedElement.value === 4) {
+    leftPadding.value = `${navigation.value.clientWidth - 75}px`;
+  }
+};
+
+window.addEventListener('resize', resizeHandler);
+
+onUnmounted(() => {
+  window.removeEventListener('resize', resizeHandler);
+});
 </script>
 
 <style lang="scss" scoped>
@@ -65,8 +80,11 @@ const leftPadding = computed(() => {
     z-index: 10;
     border-radius: 100%;
     border: 5px solid white;
-    transition: 2s all;
     left: v-bind(leftPadding);
+
+    &_animation {
+      transition: 2s all;
+    }
   }
 }
 </style>
